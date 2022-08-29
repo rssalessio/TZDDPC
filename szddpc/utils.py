ddcf0a6753af2c83eec856bdc030e39a4e2d8a77
Alessio Russo <alessior@kth.se>
2022-08-29 11:01:57 +0200

    add check on robustness of K based on probabilistic guarantees

--- szddpc/utils.py
+++ szddpc/utils.py
@@ -9,25 +9,25 @@ def spectral_radius(X: np.ndarray) -> float:
     assert len(X.shape) == 2 and X.shape[0] == X.shape[1], 'X is not  a square matrix'
     return np.abs(np.linalg.eigvals(X)).max()
 
-def compute_A_B(Msigma: MatrixZonotope, K: np.ndarray, num_init: int = 10) -> Tuple[np.ndarray, np.ndarray]:
+def compute_A_B(Mdata: MatrixZonotope, K: np.ndarray, num_init: int = 10) -> Tuple[np.ndarray, np.ndarray]:
     """ Computes the adversarial matrices (A,B) for a given control gain K and set of matrices M """
     dim_u, dim_x = K.shape
     A = cp.Variable((dim_x, dim_x))
     B = cp.Variable((dim_x, dim_u))
 
-    beta_A = cp.Variable(Msigma.num_generators)
-    beta_B = cp.Variable(Msigma.num_generators)
+    beta_A = cp.Variable(Mdata.num_generators)
+    beta_B = cp.Variable(Mdata.num_generators)
 
     constraints = [
         beta_A >= -1, beta_A <= 1,
         beta_B >= -1, beta_B <= 1
     ]
 
-    Agen = Msigma.center[:, :dim_x]
-    Bgen = Msigma.center[:, dim_x:]
-    for i in range(Msigma.num_generators):
-        Agen = Agen + Msigma.generators[i][:, :dim_x] * beta_A[i]
-        Bgen = Bgen + Msigma.generators[i][:, dim_x:] * beta_B[i]
+    Agen = Mdata.center[:, :dim_x]
+    Bgen = Mdata.center[:, dim_x:]
+    for i in range(Mdata.num_generators):
+        Agen = Agen + Mdata.generators[i][:, :dim_x] * beta_A[i]
+        Bgen = Bgen + Mdata.generators[i][:, dim_x:] * beta_B[i]
 
     constraints.append(A == Agen)
     constraints.append(B == Bgen)
@@ -56,8 +56,16 @@ def compute_control_gain(A: np.ndarray, B: np.ndarray) -> np.ndarray:
     # Return K
     return Z.value @ np.linalg.inv(X.value)
 
-def compute_theta(Msigma: MatrixZonotope, A0: np.ndarray, B0: np.ndarray, tolerance: float = 1e-5, initial_points: int = 10, max_iterations: int = 20) -> Theta:
-    assert Msigma.contains(np.hstack([A0,B0])), 'M does not contain (A0,B0)'
+def compute_theta(
+        Mdata: MatrixZonotope,
+        A0: np.ndarray,
+        B0: np.ndarray,
+        tolerance: float = 1e-5,
+        initial_points: int = 10,
+        max_iterations: int = 20,
+        accuracy: float = 1e-2,
+        confidence: float = 1e-5) -> Theta:
+    assert Mdata.contains(np.hstack([A0,B0])), 'Mdata does not contain (A0,B0)'
     dim_x, dim_u = B0.shape
 
     An = A0.copy()
@@ -72,7 +80,7 @@ def compute_theta(Msigma: MatrixZonotope, A0: np.ndarray, B0: np.ndarray, tolera
         Kn = compute_control_gain(An, Bn)
         lambda_adv = spectral_radius(An + Bn @ Kn)
         
-        An, Bn = compute_A_B(Msigma, Kn, initial_points)
+        An, Bn = compute_A_B(Mdata, Kn, initial_points)
         
         lambda_max = max(spectral_radius(An + Bn @ Kn), spectral_radius(A0+B0@Kn))
         print(f'[Iteration {iteration}] Closed loop spectral radius: {lambda_init}->{lambda_max} - Adversarial spectral radius: {lambda_adv} - K {Kn.flatten()}')
@@ -86,5 +94,31 @@ def compute_theta(Msigma: MatrixZonotope, A0: np.ndarray, B0: np.ndarray, tolera
     print(f'Optimization completed. Closed loop spectral radius: {lambda_max} - K {Kn.flatten()}')
     print('--------------------------------------------')
 
-    assert Msigma.contains(np.hstack([An,Bn])), 'M does not contain (An,Bn)'
+    assert Mdata.contains(np.hstack([An,Bn])), 'M does not contain (An,Bn)'
+    assert is_gain_robust(Mdata, Kn, accuracy, confidence), f'K is not robust with accuracy-confidence of {accuracy,1-confidence}'
+
     return Theta(Kn, An - A0, Bn - B0)
+
+def is_gain_robust(Mdata: MatrixZonotope, K: np.ndarray, accuracy: float, confidence: float) -> bool:
+    """
+    Verifies that a certain gain K is robust for given (accuracy, confidence) levels. Robustness is guaranteed
+    w.r.t. a uniform distribution over Mdata.
+
+    Returns True if the gain is robust
+    """
+    assert K.shape[1] == Mdata.shape[0], 'Wrong dimensionality for K'
+    assert accuracy > 0 and accuracy < 1, 'Accuracy should be in (0,1)'
+    assert confidence > 0 and confidence < 1, 'confidence should be in (0,1)'
+
+    dim_n = K.shape[1]
+
+    N = np.log(1/confidence) / np.log(1 / (1 - accuracy))
+
+    for n in range(N):
+        X = Mdata.sample()
+        A, B = X[:, :dim_n], X[:, dim_n:]
+
+        if spectral_radius(A + B @K ) >= 1.:
+            return False
+    
+    return True
