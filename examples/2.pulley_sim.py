16ac1ef61690aac2589ebad3af4bdd024bfba1d5
Alessio Russo <russo.alessio@outlook.com>
2022-11-23 00:49:12 +0100

    add data and results

--- examples/2.pulley_sim.py
+++ examples/2.pulley_sim.py
@@ -3,27 +3,23 @@ import numpy as np
 import scipy.signal as scipysig
 import cvxpy as cp
 import matplotlib.pyplot as plt
-
+import time
 from typing import List
 from cvxpy.expressions.expression import Expression
 from cvxpy.constraints.constraint import Constraint
-from tzddpc import SZDDPC, Data, SystemZonotopes
+from tzddpc import TZDDPC, Data, SystemZonotopes
 from utils import generate_trajectories
 from pyzonotope import Zonotope
-
+from pyzpc import ZPC, Data as DataZPC, SystemZonotopes as ZonotopesZPC
 from scipy.linalg import solve_discrete_are
 
 # Define the loss function
 def loss_callback(u: cp.Variable, y: cp.Variable) -> Expression:
     horizon, M, P = u.shape[0], u.shape[1], y.shape[1]
-    ref = np.array([[1,0,0,0]]*horizon)
-    # import pdb
-    # pdb.set_trace()
-    # Sum_t ||y_t - r_t||^2
     cost = 0
     for i in range(horizon):
-        cost += 1*cp.norm(y[i,0])
-    return  cost #100*cp.sum(cp.norm(y[1:] - ref, p=2, axis=1))
+        cost += cp.norm(y[i,0]-1,p=2)
+    return  cost
 
 # Define additional constraints
 def constraints_callback(u: cp.Variable, y: cp.Variable) -> List[Constraint]:
@@ -46,6 +42,10 @@ den = [1, -1.41833, 1.58939, -1.31608, 0.88642]
 sys = scipysig.TransferFunction(num, den, dt=dt).to_ss()
 dim_x, dim_u = sys.B.shape
 
+zpc_times = []
+tzddpc_times = []
+zpc_data = []
+tzddpc_data = []
 
 # Define zonotopes and generate data
 X0 = Zonotope([0] * dim_x, 0. * np.ones((dim_x,1)))
@@ -53,101 +53,103 @@ U = Zonotope([1] * dim_u, 3 * np.ones((dim_u,1)))
 W = Zonotope([0] * dim_x, 1e-1* np.ones((dim_x, 1)))
 X = Zonotope([1] * dim_x, 2*np.ones((dim_x, 1)))
 zonotopes = SystemZonotopes(X0, U, X, W)
-
+W_vertices = W.compute_vertices()
+num_W_vertices = len(W_vertices)
 num_trajectories = 1
 num_steps_per_trajectory = 400
-horizon =10
-
-data = generate_trajectories(sys, X0, U, W,  num_trajectories, num_steps_per_trajectory)
-
-# Build DPC
-szddpc = SZDDPC(data)
-szddpc.build_zonotopes_theta(zonotopes)
-x0 = X0.sample().flatten()
 
 
-x = [x0]
-xbar = [x[-1].copy()]
-e = [np.zeros_like(x[-1])]
-Ze = [Zonotope(np.zeros(dim_x), np.zeros((dim_x,1))) + x[-1]]
+for id_run in range(5):
+    data = generate_trajectories(sys, X0, U, W,  num_trajectories, num_steps_per_trajectory)
 
-szddpc.build_problem(2, loss_callback, constraints_callback)
+    # Build TZDDPC
+    tzddpc = TZDDPC(data)
+    tzddpc.build_zonotopes_theta(zonotopes)
+    x0 = X0.sample().flatten()
 
-for t in range(40):
-    result, v, xbark, Zek = szddpc.solve(
-        xbar[-1],
-        e[-1],
-        verbose=False
-    )
-    print(f'[{t}] x: {x[-1]} - xbar: {xbar[-1]} - v: {v[0]}')
-
-    xbar.append(xbark[1])
-    u = szddpc.theta.K @ x[-1] + v[0]
-    x_next = sys.A @ x[-1] +  np.squeeze(sys.B @ u) + W.sample()
-    x.append(x_next.flatten())
-    e.append(x[-1] - xbar[-1])
-    szddpc.theta.K 
-    Zek = Zek.Z.value
-    Ze.append(Zonotope(Zek[:, 0], Zek[:, 1:]) + xbar[-1])
+    total_steps  = 200
+    x = [x0]
+    xbar = [x[-1].copy()]
+    e = [np.zeros_like(x[-1])]
+    Ze = [Zonotope(np.zeros(dim_x), np.zeros((dim_x,1))) + x[-1]]
+    N = 2
 
 
-x_tzddpc = np.array(x)
-x = [x0]
+    
+    start_time_tzddpc = time.time()
+    tzddpc.build_problem(N, loss_callback, constraints_callback)
+    for t in range(total_steps):
+        result, v, xbark, Zek = tzddpc.solve(
+            xbar[-1],
+            e[-1],
+            verbose=False
+        )
+        # print(f'[{t}] x: {x[-1]} - xbar: {xbar[-1]} - v: {v[0]}')
+        
 
-Acenter, Bcenter = szddpc.Mdata.center[:, :dim_x], szddpc.Mdata.center[:, dim_x:]
-P = solve_discrete_are(Acenter, Bcenter, np.eye(dim_x), np.eye(dim_u))
-Klqr = -np.linalg.inv(np.eye(dim_u) + Bcenter.T @ P @ Bcenter) @ (Bcenter.T @ P @ Acenter)
-for t in range(40):
-    u = Klqr @ x[-1]# + 1
-    x_next = sys.A @ x[-1] +  np.squeeze(sys.B @ u) + W.sample()
-    x.append(x_next.flatten())
+        xbar.append(xbark[1])
+        u = tzddpc.theta.K @ e[-1] + v[0]
+        x_next = sys.A @ x[-1] +  np.squeeze(sys.B @ u) + W.sample()#[np.random.choice(num_W_vertices)]
+        x.append(x_next.flatten())
+        e.append(x[-1] - xbar[-1])
+        
+        Zek = Zek.Z.value
+    end_time_tzddpc = time.time() - start_time_tzddpc
+    
 
-x_lqr = np.array(x)
+    x_tzddpc = np.array(x)
+    tzddpc_data.append(x_tzddpc)
+    tzddpc_times.append(end_time_tzddpc)
+    np.save(f'results/pulley.xtzddpc.{id_run}', x_tzddpc)
+    x = [x0]
+
+    # Acenter, Bcenter = tzddpc.Mdata.center[:, :dim_x], tzddpc.Mdata.center[:, dim_x:]
+    # P = solve_discrete_are(Acenter, Bcenter, np.eye(dim_x), np.eye(dim_u))
+    # Klqr = -np.linalg.inv(np.eye(dim_u) + Bcenter.T @ P @ Bcenter) @ (Bcenter.T @ P @ Acenter)
+    # for t in range(total_steps):
+    #     u = Klqr @ x[-1]# + 1
+    #     x_next = sys.A @ x[-1] +  np.squeeze(sys.B @ u) + W.sample()
+    #     x.append(x_next.flatten())
+
+    # x_lqr = np.array(x)
+
+
+
+
+    # BUILD ZPC
+    print('BUILDING ZPC')
+    zpc_zonotopes = ZonotopesZPC(X0, U, X, W, Zonotope(np.zeros(dim_x), np.zeros((dim_x,1))), Zonotope(np.zeros(dim_x), np.zeros((dim_x,1))))
+    zpc = ZPC(DataZPC(data.u, data.x))
+
+
+    start_time_zpc = time.time()
+    problem = zpc.build_problem(zpc_zonotopes, N, loss_callback, constraints_callback)
+    x = [x0]
+
+    for n in range(total_steps):
+        #print(f'Solving step {n}')
+        result, info = zpc.solve(x[-1], verbose=False,warm_start=True)
+        u = info['u_optimal']
+        x_next = sys.A @ x[-1] +  np.squeeze(sys.B @ u[0]) + W.sample()# W_vertices[np.random.choice(num_W_vertices)]
+        x.append(x_next.flatten())
+    end_time_zpc = time.time() - start_time_zpc
+    x_zpc = np.array(x)
+
+
+    zpc_times.append(end_time_zpc)
+    zpc_data.append(x_zpc)
+
+    np.save(f'results/pulley.xzpc.{id_run}', x_zpc)
+    print(f'ZPC: {round(end_time_zpc/60,2)}')
+    print(f'TZDDPC: {round(end_time_tzddpc/60,2)}')
+
+np.save('results/pulley.xzpc', zpc_data)
+np.save('results/pulley.tzddpc', tzddpc_data)
+np.save('results/pulley.zpc_times', zpc_times)
+np.save('results/pulley.tzddpc_times', tzddpc_times)
 plt.plot(x_tzddpc[:,0], label=f'TZDDPC - $x_1$')
-plt.plot(x_lqr[:,0], label=f'LQR - $x_1$')
+plt.plot(x_zpc[:,0], label=f'ZPC - $x_1$')
 plt.grid()
 plt.legend()
 plt.show()
-# print(info)
-# plt.figure()
-# plt.margins(x=0, y=0)
 
-# # Simulate for different values of T
-# for T in T_list:
-#     sys.reset()
-#     # Generate initial data and initialize DeePC
-#     data = sys.apply_input(u = np.random.normal(size=T).reshape((T, 1)), noise_std=0)
-#     deepc = DeePC(data, Tini = T_INI, horizon = HORIZON)
-
-#     # Create initial data
-#     data_ini = Data(u = np.zeros((T_INI, 1)), y = np.zeros((T_INI, 1)))
-#     sys.reset(data_ini = data_ini)
-
-#     deepc.build_problem(
-#         build_loss = loss_callback,
-#         build_constraints = constraints_callback,
-#         lambda_g = LAMBDA_G_REGULARIZER,
-#         lambda_y = LAMBDA_Y_REGULARIZER)
-
-#     for idx in range(300):
-#         # Solve DeePC
-#         u_optimal, info = deepc.solve(data_ini = data_ini, warm_start=True)
-
-
-#         # Apply optimal control input
-#         _ = sys.apply_input(u = u_optimal[:s, :], noise_std=0)
-
-#         # Fetch last T_INI samples
-#         data_ini = sys.get_last_n_samples(T_INI)
-
-#     # Plot curve
-#     data = sys.get_all_samples()
-#     plt.plot(data.y[T:], label=f'$s={s}, T={T}, T_i={T_INI}, N={HORIZON}$')
-
-# plt.ylim([0, 2])
-# plt.xlabel('Step')
-# plt.ylabel('y')
-# plt.title('Closed loop output')
-# plt.legend(fancybox=True, shadow=True)
-# plt.grid()
-# plt.show()
--- examples/3.5dimsystem_sim.py
+++ examples/3.5dimsystem_sim.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from typing import List
 from cvxpy.expressions.expression import Expression
 from cvxpy.constraints.constraint import Constraint
-from tzddpc import SZDDPC, Data, SystemZonotopes
+from tzddpc import TZDDPC, Data, SystemZonotopes
 from utils import generate_trajectories
 from pyzonotope import Zonotope
 
@@ -16,14 +16,14 @@ def loss_callback(u: cp.Variable, x: cp.Variable) -> cp.Expression:
 
     cost = 0
     for i in range(horizon):
-        cost += cp.norm(x[i,1] - 1,p=2) +  1e-1*cp.norm(u[i] - 8, p=2)
+        cost += 1e9* cp.norm(x[i,1]-2,p=2) +  1e-1*cp.norm(u[i], p=2)
     return  cost
 
 
 def constraints_callback(u: cp.Variable, x: cp.Variable) -> List[Constraint]:
     horizon, dim_u, dim_x = u.shape[0], u.shape[1], x.shape[1]
     # Define a list of additional input/output constraints
-    return []#[x[:, 1] <= 10, x[:,1] >=0]#x >= -2, x <= 4, u >= -6, u <= 6.]
+    return [x[:, 1] <= 10, x[:,1] >=2] #, x >= -2, x <= 4, u >= -6, u <= 6.]
 
 
 A = np.array(
@@ -43,7 +43,10 @@ sys = scipysig.StateSpace(A,B,C,D)
 X0 = Zonotope([-2, 4, 3, -2.5, 5.5], 0 * np.diag([1] * dim_x))
 U = Zonotope([7] * dim_u,  100 * np.diag([1] * dim_u))
 W = Zonotope([0] * dim_x, 0.1* np.ones((dim_x, 1)))
-X = Zonotope([1] * dim_x, 100 * np.ones((dim_x, 1)))
+
+Id = 20 * np.ones((dim_x, 1))
+Id[1] = 19
+X = Zonotope([1, 20, 1, 1, 1], Id)
 W_vertices = W.compute_vertices()
 num_W_vertices = len(W_vertices)
 zonotopes = SystemZonotopes(X0, U, X, W)
@@ -55,8 +58,8 @@ horizon =10
 data = generate_trajectories(sys, X0, U, W,  num_trajectories, num_steps_per_trajectory)
 
 # Build DPC
-szddpc = SZDDPC(data)
-szddpc.build_zonotopes_theta(zonotopes)
+tzddpc = TZDDPC(data)
+tzddpc.build_zonotopes_theta(zonotopes)
 x0 = X0.sample().flatten()
 
 
@@ -65,20 +68,20 @@ xbar = [x[-1].copy()]
 e = [np.zeros_like(x[-1])]
 Ze = [Zonotope(np.zeros(dim_x), np.zeros((dim_x,1))) + x[-1]]
 
-szddpc.build_problem(2, loss_callback, constraints_callback)
+tzddpc.build_problem(2, loss_callback, constraints_callback)
 
-for t in range(100):
-    result, v, xbark, Zek = szddpc.solve(
+for t in range(200):
+    result, v, xbark, Zek = tzddpc.solve(
         xbar[-1],
         e[-1],
         verbose=False
     )
+    print(f'[{t}] x: {x[-1]} - xbar: {xbar[-1]} - v: {v[0]}')
     
 
     xbar.append(xbark[1])
-    u = szddpc.theta.K @ x[-1] + v[0]
-    print(f'[{t}] x: {x[-1]} - xbar: {xbar[-1]} - v: {v[0]} - u: {u} - ubar: {szddpc.theta.K @ xbar[-1] + v[0]} - Ke: {szddpc.theta.K @ e[-1]} - Kx {szddpc.theta.K @ x[-1] }')
-    x_next = sys.A @ x[-1] +  np.squeeze(sys.B @ u) + W_vertices[np.random.choice(num_W_vertices)]# W.sample()
+    u = tzddpc.theta.K @ e[-1] + v[0]
+    x_next = sys.A @ x[-1] +  np.squeeze(sys.B @ u) + W.sample()# W_vertices[np.random.choice(num_W_vertices)]
     x.append(x_next.flatten())
     e.append(x[-1] - xbar[-1])
     
@@ -91,46 +94,5 @@ plt.plot(x[:,1], label=f'$x_1$')
 plt.grid()
 plt.legend()
 plt.show()
-# print(info)
-# plt.figure()
-# plt.margins(x=0, y=0)
-
-# # Simulate for different values of T
-# for T in T_list:
-#     sys.reset()
-#     # Generate initial data and initialize DeePC
-#     data = sys.apply_input(u = np.random.normal(size=T).reshape((T, 1)), noise_std=0)
-#     deepc = DeePC(data, Tini = T_INI, horizon = HORIZON)
-
-#     # Create initial data
-#     data_ini = Data(u = np.zeros((T_INI, 1)), y = np.zeros((T_INI, 1)))
-#     sys.reset(data_ini = data_ini)
-
-#     deepc.build_problem(
-#         build_loss = loss_callback,
-#         build_constraints = constraints_callback,
-#         lambda_g = LAMBDA_G_REGULARIZER,
-#         lambda_y = LAMBDA_Y_REGULARIZER)
-
-#     for idx in range(300):
-#         # Solve DeePC
-#         u_optimal, info = deepc.solve(data_ini = data_ini, warm_start=True)
-
-
-#         # Apply optimal control input
-#         _ = sys.apply_input(u = u_optimal[:s, :], noise_std=0)
-
-#         # Fetch last T_INI samples
-#         data_ini = sys.get_last_n_samples(T_INI)
-
-#     # Plot curve
-#     data = sys.get_all_samples()
-#     plt.plot(data.y[T:], label=f'$s={s}, T={T}, T_i={T_INI}, N={HORIZON}$')
-
-# plt.ylim([0, 2])
-# plt.xlabel('Step')
-# plt.ylabel('y')
-# plt.title('Closed loop output')
-# plt.legend(fancybox=True, shadow=True)
-# plt.grid()
-# plt.show()
+import pdb
+pdb.set_trace()
